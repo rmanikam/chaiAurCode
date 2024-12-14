d75103a15f848b2367faa42dbde704ad529fbdcb
Raman Sahi <ramansahi@Ramans-MacBook-Air.local>
2024-12-14 06:02:41 +0530

    first commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS
+//code as newer version
+
+// alert("hello"); // we are using nodejs and nott browser
+
+console.log(3 + 3);
+// code readability should be high
+
+console.log("Hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+
+// bigint
+
+// string => ""
+
+// boolean => true/false
+
+// null => standalone value
+
+// undefined =>
+
+// symbol => unique
+
+// object
+
+console.log(typeof "raman");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
